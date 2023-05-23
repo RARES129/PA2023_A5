530ae47372258ac41e1a3d2b51058da498ca6242
RARES129 <dascaluraresm@gmail.com>
2023-05-23 16:44:45 +0300

    ,

---
+++ LAB12/Compulsory/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\dasca\\Desktop\\GREEN_GUM\\PA2023_A5\\LAB12\\Compulsory\\src\\main\\java\\org\\example\\DummyClass.class";
+        var analyzer = new Analyzer(path);
+
+        System.out.println();
+        System.out.println("Hello world!");
+    }
+}
